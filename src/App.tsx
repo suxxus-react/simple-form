2aea9a8b0c7edc5fb51dd9de472c05963da0251b
d <xx@x.com>
2023-02-06 11:20:32 +0100

    Wip 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,116 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { useState, useEffect } from "react";
+
+type Fruit = string;
+
+type Model = {
+  fruits: Fruit[];
+  selected: string[];
+};
+
+type Ui = Model & {
+  dispatch: React.Dispatch<React.SetStateAction<Msg>>;
+};
+
+type SelectFruit = { type: "SelectFruit"; fruit: Fruit };
+
+type UnselectFruit = { type: "UnselectFruit"; fruit: Fruit };
+
+type NoOp = { type: "NoOp" };
+
+type Msg = SelectFruit | UnselectFruit | NoOp;
+
+function getSelected(fruit: Fruit, selected: Fruit[]) {
+  return selected.includes(fruit);
+}
+
+function doSelectFruit(
+  fruit: Fruit,
+  selected: Fruit[],
+  maxsize: number,
+): Fruit[] {
+  return selected.length >= maxsize
+    ? [fruit, ...selected].filter((_, i, xs) => i !== xs.length - 1)
+    : [fruit, ...selected];
+  //
+}
+
+function doUnselectFruit(fruit: Fruit, selected: Fruit[]): Fruit[] {
+  return selected.filter((f) => f !== fruit);
+}
+
+function Form({ fruits, selected, dispatch }: Ui): JSX.Element {
+  return (
+    <form>
+      <ul>
+        {fruits.map((fruit) => (
+          <li key={fruit}>
+            <label htmlFor={fruit}>{fruit}</label>
+            <input
+              id={fruit}
+              type="checkbox"
+              checked={getSelected(fruit, selected)}
+              onChange={(evt) => {
+                evt.preventDefault();
+                const msg = evt.target.checked
+                  ? "SelectFruit"
+                  : "UnselectFruit";
+                dispatch({ type: msg, fruit: evt.target.id });
+              }}
+            />
+          </li>
+        ))}
+      </ul>
+    </form>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  const intialValue: Model = {
+    fruits: ["apple", "banana", "apricot", "watermelon", "grapes"],
+    selected: [],
+  };
+
+  const maxsize = 2;
+
+  const [model, setModel] = useState<Model>(intialValue);
+  const [msg, setMsg] = useState<Msg>({ type: "NoOp" });
+
+  useEffect(() => {
+    //
+    switch (msg.type) {
+      case "SelectFruit":
+        setModel({
+          ...model,
+          selected: doSelectFruit(msg.fruit, [...model.selected], maxsize),
+        });
+        break;
+      case "UnselectFruit":
+        setModel({
+          ...model,
+          selected: doUnselectFruit(msg.fruit, [...model.selected]),
+        });
+        break;
+      case "NoOp":
+        break;
+    }
+  }, [msg]);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>fruits</h2>
+      <Form dispatch={setMsg} {...model} />
     </div>
   );
 }
+
+/*
+selectedFruits = [banana, apple],
+
+
+[{fruit: string, selected: boolean}]
+
+
+*/
 
 export default App;
